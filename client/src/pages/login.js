a0424c04d959d2f48d1a859ac64ef0e1b498f903
Jonathan Woodruff <jonathanmwoodruff@gmail.com>
2024-04-04 09:46:48 -0500

    minor changes to login

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -162,11 +162,6 @@ const Login = () => {
                   Log In
                 </Button>
                 <Grid container>
-                  <Grid item xs>
-                    <Link href="/request-reset" variant="body2">
-                      Forgot password?
-                    </Link>
-                  </Grid>
                   <Grid item>
                     <Link href="/register" variant="body2">
                       {"Don't have an account? Sign Up"}
